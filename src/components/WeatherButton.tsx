0b92e65f67cb7e3a76f58a6a175b84782b893c0f
IdleScV <weschen1996@gmail.com>
2022-07-12 21:35:10 -0400

    exceeded rate limit on this api

--- src/components/WeatherButton.tsx
+++ src/components/WeatherButton.tsx
@@ -1,19 +1,122 @@
-import { Button, VStack, Text, Icon, HStack } from "@chakra-ui/react";
+import { Button, VStack, Text, Icon, HStack, Box } from "@chakra-ui/react";
 import { motion } from "framer-motion";
+import { useState } from "react";
+import { useEffect } from "react";
+import "weather-react-icons/lib/css/weather-icons.css";
 import { TiWeatherCloudy } from "react-icons/ti";
 import { Link } from "react-router-dom";
+import { WeatherIcon } from "weather-react-icons";
+
+interface WeatherData {
+    city: {
+        id: number;
+        name: string;
+        country: string;
+        coord: {
+            lat: number;
+            lon: number;
+        };
+        timezone: number;
+        sunrise: number;
+        sunset: number;
+    };
+    cnt: number;
+    cod: string;
+    list: Array<{
+        clouds: {
+            all: number;
+        };
+        dt: number;
+        dt_txt: string;
+        main: {
+            feels_like: number;
+            humidity: number;
+            pressure: number;
+            temp: number;
+            temp_max: number;
+            temp_min: number;
+        };
+        pop: number;
+        sys: {
+            pod: string;
+        };
+        visibility: number;
+        weather: Array<{
+            description: string;
+            icon: string;
+            id: number;
+            main: string;
+        }>;
+        wind: {
+            speed: number;
+            deg: number;
+            gust: number;
+        };
+    }>;
+}
+
 const WeatherButton = () => {
-  return (
-    <Button flex={1} height="100%" as={Link} to="/weather">
-      <VStack spacing={2}>
-        {/* change icon depending on current weather */}
-        <HStack as={motion.div} layoutId="weather-text">
-          <Icon as={TiWeatherCloudy} fontSize="4xl" />
-          <Text fontSize="4xl">Weather</Text>
-        </HStack>
-      </VStack>
-    </Button>
-  );
+    const [currentConditions, setCurrentConditions] = useState<WeatherData>();
+    // const convertKelvinToFahrenheit = (kelvin?: number) => {
+    //     if (kelvin) {
+    //         return Math.round((kelvin * 9) / 5 - 459.67);
+    //     }
+    //     return "loading";
+    // };
+    const options = {
+        method: "GET",
+        headers: {
+            "X-RapidAPI-Key":
+                "32b9f13634msha69678ce2655e1ap1711c9jsn8befea941d17",
+            "X-RapidAPI-Host": "community-open-weather-map.p.rapidapi.com",
+        },
+    };
+    useEffect(() => {
+        fetch(
+            "https://community-open-weather-map.p.rapidapi.com/forecast?q=reston%2Cus",
+            options
+        )
+            .then((response) => response.json())
+            .then((response) => {
+                console.log(response);
+                setCurrentConditions(response);
+            })
+            .catch((err) => console.error(err));
+    }, []);
+    if (currentConditions) {
+        return (
+            <Button flex={1} height="100%" as={Link} to="/weather">
+                <VStack spacing={2}>
+                    {/* change icon depending on current weather */}
+                    <HStack as={motion.div} layoutId="weather-text">
+                        <WeatherIcon iconId={802} name="owm" night />
+                        <Text fontSize="4xl">Weather</Text>
+                    </HStack>
+                    <HStack as={motion.div}>
+                        <VStack>
+                            <Text fontSize="lg">
+                                feels like:
+                                {currentConditions?.list[0]?.main?.feels_like}
+                            </Text>
+                        </VStack>
+                        {/* <VStack>
+                        <Text>
+                            precipitation: {currentConditions?.list[0].pop}
+                        </Text>
+                        <Text>
+                            humidity: {currentConditions?.list[0].main.humidity}
+                            %
+                        </Text>
+                        <Text>
+                            wind: {currentConditions?.list[0].wind.speed} mph
+                        </Text>
+                    </VStack> */}
+                    </HStack>
+                </VStack>
+            </Button>
+        );
+    }
+    return <></>;
 };
 
 export default WeatherButton;
--- src/views/Weather.tsx
+++ src/views/Weather.tsx
@@ -5,30 +5,30 @@ import { TiWeatherCloudy } from "react-icons/ti";
 import { Link } from "react-router-dom";
 
 const Weather = () => {
-  console.log("Weather", process.env.REACT_APP_WEATHER_API_KEY);
-  return (
-    <VStack spacing={0} height="100vh" p={2}>
-      <HStack justifyContent="space-between" width="100%" flex={1} px={4}>
-        <IconButton
-          aria-label="back"
-          icon={<ArrowBackIcon />}
-          as={Link}
-          to="/"
-        />
-        <HStack as={motion.div} layoutId="weather-text">
-          <Icon as={TiWeatherCloudy} fontSize="4xl" />
-          <Text fontSize="4xl">Weather</Text>
-        </HStack>
-        <IconButton
-          aria-label="weather-settings"
-          icon={<SettingsIcon />}
-          as={Link}
-          to="/settings/weather"
-        />
-      </HStack>
-      <HStack spacing={4} p={2} flex={9} width="100%"></HStack>
-    </VStack>
-  );
+    console.log("Weather", process.env.REACT_APP_WEATHER_API_KEY);
+    return (
+        <VStack spacing={0} height="100vh" p={2}>
+            <HStack justifyContent="space-between" width="100%" flex={1} px={4}>
+                <IconButton
+                    aria-label="back"
+                    icon={<ArrowBackIcon />}
+                    as={Link}
+                    to="/"
+                />
+                <HStack as={motion.div} layoutId="weather-text">
+                    <Icon as={TiWeatherCloudy} fontSize="4xl" />
+                    <Text fontSize="4xl">Weather</Text>
+                </HStack>
+                <IconButton
+                    aria-label="weather-settings"
+                    icon={<SettingsIcon />}
+                    as={Link}
+                    to="/settings/weather"
+                />
+            </HStack>
+            <HStack spacing={4} p={2} flex={9} width="100%"></HStack>
+        </VStack>
+    );
 };
 
 export default Weather;
